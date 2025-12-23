53616d249270c9d82928bad697dc07dfe576c7f5
Pavlo Kolodka <pavlo.kolodka@iccswitch.com>
2025-12-23 18:16:35 +0200

    install libraries

---
+++ test_install.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+import keras
+import torch
+
+print("TensorFlow version:", tf.__version__)
+print("Keras version:", keras.__version__)
+print("PyTorch version:", torch.__version__)
